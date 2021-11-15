890ee66750de2c7b12d07ffbddd993e41dc7b232
asadbekasadbek <aa-asad16@mail.ru>
2021-11-15 12:42:53 +0500

    new blade and node_modle

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/a', function () {
+    return view('productes.product');
 });

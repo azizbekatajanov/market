6fb353a76b9046203931caa2ca9184979d74edfa
azizbekatajanov <azizbekatajanov1998@gmail.com>
2021-11-23 16:54:24 +0500

    some bugs fixed

--- app/Http/Controllers/Api/V1/CategoryController.php
+++ app/Http/Controllers/Api/V1/CategoryController.php
@@ -8,7 +8,6 @@ use Illuminate\Http\Request;
 use App\Http\Resources\CategoryResource;
 
 class CategoryController extends Controller
-class CategoryController extends Controller
 {
     /**
      * Display a listing of the resource.

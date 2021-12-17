b4f919758f8215d4eaa8841d970abad2972119de
asadbekasadbek <aa-asad16@mail.ru>
2021-12-17 15:09:43 +0500

    delete locolizesion

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,34 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('lang/{lang}', ['as' => 'lang.switch', 'uses' => 'App\Http\Controllers\LanguageController@switchLang']);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

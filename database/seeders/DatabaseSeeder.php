70654b215eb56c3ff4dedce3c99bdeb2476494ec
azizbekatajanov <azizbekatajanov1998@gmail.com>
2021-12-17 10:37:45 +0500

    Brand Factory added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Brand;
 use App\Models\Category;
 use Illuminate\Database\Seeder;
 
@@ -16,5 +17,6 @@ class DatabaseSeeder extends Seeder
     {
          \App\Models\User::factory(50)->create();
          Category::factory(30)->create();
+         Brand::factory(30)->create();
     }
 }

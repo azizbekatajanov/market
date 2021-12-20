b2a1504df0eca064630468ae757bad11c5c6488b
Асад Исамахаматов <AIsamaxamatov@paymart.local>
2021-12-20 11:39:26 +0500

    new Localization middleware new Rout

--- resources/views/сomponents/сomponent.blade.php
+++ resources/views/сomponents/сomponent.blade.php
@@ -60,11 +60,8 @@
                             <span class="flag-icon flag-icon-{{Config::get('languages')[App::getLocale()]['flag-icon']}}"></span> {{ Config::get('languages')[App::getLocale()]['display'] }}
                         </a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            @foreach (Config::get('languages') as $lang => $language)
-                                @if ($lang != App::getLocale())
-                                    <a class="dropdown-item" href="{{ route('lang.switch', $lang) }}"><span class="flag-icon flag-icon-{{$language['flag-icon']}}"></span> {{$language['display']}}</a>
-                                @endif
-                            @endforeach
+                                    <a class="dropdown-item" href="/setlocale/ru"><span class="flag-icon flag-icon-ru"></span> ru</a>
+                                    <a class="dropdown-item" href="/setlocale/en"><span class="flag-icon flag-icon-us"></span> en</a>
                         </div>
                     </li>
                     <h3 style="color: #fff3cd">{{__('messages.welcome')}}</h3>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Redirect;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,7 +13,50 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get(
+    'setlocale/{lang}',
+    function ($lang) {
 
+        $referer = Redirect::back()->getTargetUrl(); //URL предыдущей страницы
+        $parse_url = parse_url($referer, PHP_URL_PATH); //URI предыдущей страницы
+
+        //разбиваем на массив по разделителю
+        $segments = explode('/', $parse_url);
+
+        //Если URL (где нажали на переключение языка) содержал корректную метку языка
+        if (in_array($segments[1], App\Http\Middleware\Locale::$languages)) {
+
+            unset($segments[1]); //удаляем метку
+        }
+
+        //Добавляем метку языка в URL (если выбран не язык по-умолчанию)
+        if ($lang != App\Http\Middleware\Locale::$mainLanguage) {
+            array_splice($segments, 1, 0, $lang);
+        }
+
+        //формируем полный URL
+        $url = Request::root() . implode("/", $segments);
+//        dd($url);
+        //если были еще GET-параметры - добавляем их
+        if (parse_url($referer, PHP_URL_QUERY)) {
+            $url = $url . '?' . parse_url($referer, PHP_URL_QUERY);
+        }
+//        dd($url);
+        $url = str_replace(env('APP_URL'),"",$url);
+        return redirect($url); //Перенаправляем назад на ту же страницу
+
+    }
+)->name('setlocale');
+
+
+Route::group(
+    [
+        'prefix'=>App\Http\Middleware\Locale::getLocale()
+    ],function ()
+    {
+        Route::get('/',[\App\Http\Controllers\HomeController::class,'index'])->name('homepage');
+    }
+);
 
 
 

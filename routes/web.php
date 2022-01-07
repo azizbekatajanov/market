d5fe2441d2c0cf5895466532a5f644a2a6819ca5
Weed <44820183+TodoshiTakato@users.noreply.github.com>
2022-01-07 11:06:41 +0500

    Nurlan's commit after git pull origin main and making changes to routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,17 @@ use Illuminate\Support\Facades\Redirect;
 */
 
 
-Route::get('/product/{id}',[\App\Http\Controllers\HomeController::class,'product']);
-Route::get('/checkout',[\App\Http\Controllers\HomeController::class,'checkout']);
-Route::get('/store',[\App\Http\Controllers\HomeController::class,'store']);
-Route::get('/blank',[\App\Http\Controllers\HomeController::class,'blank']);
-
-Route::get('/contact',[\App\Http\Controllers\HomeController::class,'contact']);
-Route::get('/categories',[\App\Http\Controllers\HomeController::class,'categories']);
-
-Route::prefix('dashboard')->group(function (){
-    Route::view('/', 'dashboard.index')->name('dashboard.index');
-    Route::view('/categories', 'dashboard.categories.index')->name('categories.index');
+//Route::get('/product/{id}',[\App\Http\Controllers\HomeController::class,'product']);
+//Route::get('/checkout',[\App\Http\Controllers\HomeController::class,'checkout']);
+//Route::get('/store',[\App\Http\Controllers\HomeController::class,'store']);
+//Route::get('/blank',[\App\Http\Controllers\HomeController::class,'blank']);
+//
+//Route::get('/contact',[\App\Http\Controllers\HomeController::class,'contact']);
+//Route::get('/categories',[\App\Http\Controllers\HomeController::class,'categories']);
+//
+//Route::prefix('dashboard')->group(function (){
+//    Route::view('/', 'dashboard.index')->name('dashboard.index');
+//    Route::view('/categories', 'dashboard.categories.index')->name('categories.index');
 
 
 //    Route::group([
